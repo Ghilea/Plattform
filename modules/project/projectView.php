9366c9a948a6ca94825d43fdcebae3df7e0c6b37
Dennis <tougent@gmail.com>
2022-01-30 17:18:48 +0100

    feat: styling projectView

--- modules/project/projectView.php
+++ modules/project/projectView.php
@@ -1,26 +1,34 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT']."/resources/includes/header.php"); ?>
 
 <main class="project">
-	<article class="container row view">
-	<section class="container row box col-lg-4 col-md-4 col-sm-4">
-		<h2>Beskrivning</h2>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, obcaecati amet velit illo debitis est rem eligendi neque repellat quaerat quae cum atque nesciunt laboriosam quisquam iure mollitia qui architecto!</p>
-		<h2>Live</h2>
-		<p><a href="#">https://rgrgrgrgargarg.com</a></p>
-		<h2>Github</h2>
-		<p><a href="#">https://github.com/ghilea/blabla</a></p>
-	</section>
+	<div class="container view">
+		<h1>En egen CMS plattform</h1>
+		<section class="container row">
+			<article>
+				<p>Mitt senaste projekt är att bygga upp en dynamisk plattform som kommer att användas till nya projekt. Plattformen bygger på anpassningsbara moduler och CMS för att snabbare kunna få upp en bass.</p>
+				
+			</article>	<a rel="noreferrer noopener" target="_blank" href="#" class="boxBtn">Live Demo</a>	
+				<a rel="noreferrer noopener" target="_blank" href="#" class="boxBtn">GitHub</a>
+		</section>
 
-	<section class="container row box col-lg-8 col-md-8 col-sm-8">
-		
-		<header>
-			<h2>Title</h2>	
-		</header>
+		<section class="container row">
+			<img src="" alt="" width="500" height="250">
+		</section>
 
-		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi mollitia accusamus autem, repudiandae nihil facere necessitatibus dolorem dolorum ad est esse, adipisci nemo quaerat porro vero, a temporibus possimus et!</p>
+		<section>
+			<article>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi mollitia accusamus autem, repudiandae nihil facere necessitatibus dolorem dolorum ad est esse, adipisci nemo quaerat porro vero, a temporibus possimus et!</p>
+				<figure>
+					<img src="" alt="" width="300" height="350">
+					<figcaption>
 
-	</section>
-</article>
+					</figcaption>
+				</figure>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi mollitia accusamus autem, repudiandae nihil facere necessitatibus dolorem dolorum ad est esse, adipisci nemo quaerat porro vero, a temporibus possimus et!</p>
+			</article>
+			
+		</section>
+	</div>
 </main>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT']."/resources/includes/footer.php"); ?>
